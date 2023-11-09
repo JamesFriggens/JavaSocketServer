67d9038d0bfc094b72e7cba8c823bfc670455d37
James <jfriggs1877@gmail.com>
2023-11-08 22:23:28 -0500

    Now the user can add methods in the Commands class if if the method name is sent to the socket server, it will execute that method

---
+++ src/Commands.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Commands {
+    
+    public void method1() {
+        System.out.println("Method 1 is called.");
+    }
+
+    public void method2() {
+        System.out.println("Method 2 is called.");
+    }
+
+    public void method3() {
+        System.out.println("Method 3 is called.");
+    }
+
+}
+
--- src/SocketServer.java
+++ src/SocketServer.java
@@ -9,6 +9,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 import java.lang.Thread;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import src.Commands;
 
 
 public class SocketServer{
@@ -77,15 +80,61 @@ public class SocketServer{
 
                 // Read input from the client
                 clientMessage = in.readLine();
+                System.out.println(clientMessage);
 
-                if(searchKeyword(clientMessage).equals("shutdown")){
-                    // whatever you want to do
-                    // you can use out.println("your message here" to return a message to the device connected to the socket)
+                Commands commands = new Commands();
+
+                try {
+                    // Use Java reflection to find and invoke the method by name
+                    Method method = Commands.class.getMethod(clientMessage);
+                    method.invoke(commands);
+                } catch (NoSuchMethodException e) {
+                    System.err.println("Method not found: " + clientMessage);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    System.err.println("Error invoking the method: " + e.getMessage());
                 }
 
-                else{
-                    System.out.println("Message sent does not equal keyword");
-                }
+                // if(searchKeyword(clientMessage).equals("shutdown")){
+                //     // whatever you want to do
+                //     // you can use out.println("your message here" to return a message to the device connected to the socket)
+                // }
+
+                // else{
+                //     System.out.println("Message sent does not equal keyword");
+                // }
+                
+                // try {
+                //     // Load the class that contains the methods
+                //     Class<?> clazz = Class.forName("Commands");
+                //     Object obj = clazz.getDeclaredConstructor().newInstance();
+
+                //     // Find the method by name
+                //     Method method = clazz.getMethod(clientMessage);
+
+                //     // Call the method on the object
+                //     method.invoke(obj);
+                //     System.out.println("it worked!");
+                // }
+                
+                // catch (ClassNotFoundException | NoSuchMethodException | ExceptionInInitializerError e) {
+                //     System.err.println("Method not found or couldn't be executed: " + clientMessage);
+                // } catch (IllegalAccessException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // } catch (InvocationTargetException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // } catch (InstantiationException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // } catch (IllegalArgumentException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // } catch (SecurityException e) {
+                //     // TODO Auto-generated catch block
+                //     e.printStackTrace();
+                // }
+                
             } 
             
             catch (IOException e) {
